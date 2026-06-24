b4c8c5e80ce69fcf60dbe0941983fba98a9e3be1
Abubakar suraj <145049374+Aboosuraj@users.noreply.github.com>
2026-06-24 09:34:00 +0000

    Ready for deployment

--- iopn-dex/lib/tokens.ts
+++ iopn-dex/lib/tokens.ts
@@ -10,29 +10,34 @@ export const TOKENS = [
                               symbol: "WOPN",
                                   address: "0xBc022C9dEb5AF250A526321d16Ef52E39b4DBD84",
                                       decimals: 18,
+                                      native: false,
                                         },
 
                                           {
                                               symbol: "OPNT",
                                                   address: "0x2aEc1Db9197Ff284011A6A1d0752AD03F5782B0d",
                                                       decimals: 18,
+                                                      native: false,
                                                         },
 
                                                           {
                                                               symbol: "tUSDT",
                                                                   address: "0x3e01b4d892E0D0A219eF8BBe7e260a6bc8d9B31b",
                                                                       decimals: 18,
+                                                                      native: false,
                                                                         },
 
                                                                           {
                                                                               symbol: "tBNB",
                                                                                   address: "0x92cF36713a5622351c9489D5556B90B321873607",
                                                                                       decimals: 18,
+                                                                                      native: false,
                                                                                         },
 
                                                                                           {
                                                                                               symbol: "IDEX",
                                                                                                   address: "0xD5D72Ea6775D7618aFD4d8c42d2d8934638B75f9",
                                                                                                       decimals: 18,
+                                                                                                      native: false,
                                                                                                         },
                                                                                                         ] as const;
