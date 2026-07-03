9bf2751279ed0e322865850b4bfa4ac12079cf49
Abubakar suraj <145049374+Aboosuraj@users.noreply.github.com>
2026-07-03 06:41:33 +0000

    fix: remove node_modules from tracking

--- iopn-dex/hooks/useSend.ts
+++ iopn-dex/hooks/useSend.ts
@@ -2,7 +2,7 @@
 
 export function useSendTx() {
   async function sendTx(to: string, amount: string, privateKey: string) {
-    const res = await fetch("http://localhost:4000/api/send", {
+    const res = await fetch("https://iopn-dex.onrender.com", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
