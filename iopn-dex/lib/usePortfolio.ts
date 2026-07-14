3f46a5ba26f6eb3b46e674cfafb41f9f8c745a09
Abubakar suraj <145049374+Aboosuraj@users.noreply.github.com>
2026-07-14 06:16:23 +0000

    Fix portfolio token balance loading for connected wallets

--- iopn-dex/lib/usePortfolio.ts
+++ iopn-dex/lib/usePortfolio.ts
@@ -1,33 +1,118 @@
 "use client";
 
-import { useAccount, useBalance, useReadContracts } from "wagmi";
-import { ERC20_ABI } from "@/lib/erc20";
-import { TOKENS } from "@/lib/tokens";
+import {
+  useAccount,
+  useBalance,
+  useReadContracts,
+} from "wagmi";
+
+import {
+  ERC20_ABI,
+} from "@/lib/erc20";
+
+import {
+  TOKENS,
+} from "@/lib/tokens";
+
+
 
 export function usePortfolio() {
-  const { address } = useAccount();
 
-    const nativeBalance = useBalance({
-        address,
-          });
 
-            const tokenContracts = TOKENS.map((token) => ({
-                address: token.address as `0x${string}`,
-                    abi: ERC20_ABI,
-                        functionName: "balanceOf",
-                            args: [address as `0x${string}`],
-                              }));
+  const {
+    address,
+    isConnected,
+  } = useAccount();
 
-                                const tokenBalances = useReadContracts({
-                                    contracts: tokenContracts,
-                                        query: {
-                                              enabled: !!address,
-                                                  },
-                                                    });
 
-                                                      return {
-                                                          address,
-                                                              nativeBalance,
-                                                                  tokenBalances,
-                                                                    };
-                                                                    }
+
+  // Native OPN balance
+
+  const nativeBalance = useBalance({
+
+    address,
+
+    query: {
+
+      enabled:
+        !!address,
+
+    },
+
+  });
+
+
+
+
+
+  // ERC20 token balances
+
+  const tokenContracts =
+    TOKENS.map((token)=>({
+
+      address:
+        token.address as `0x${string}`,
+
+      abi:
+        ERC20_ABI,
+
+      functionName:
+        "balanceOf",
+
+      args:
+        address
+        ?
+        [
+          address
+        ]
+        :
+        undefined,
+
+
+    }));
+
+
+
+
+
+  const tokenBalances =
+    useReadContracts({
+
+      contracts:
+        tokenContracts,
+
+
+      query: {
+
+        enabled:
+          !!address &&
+          isConnected,
+
+
+        refetchInterval:
+          10000,
+
+      },
+
+
+    });
+
+
+
+
+
+
+  return {
+
+    address,
+
+    isConnected,
+
+    nativeBalance,
+
+    tokenBalances,
+
+  };
+
+
+}
